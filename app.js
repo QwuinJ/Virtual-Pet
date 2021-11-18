ada171a6f58957fff9192b12c88bd922e72e734d
QwuinJ <jackbelfield8@gmail.com>
2021-11-18 11:39:26 +0000

    added styles and started changing options

--- app.js
+++ app.js
@@ -3,9 +3,17 @@
 let pet = {
     name: " ",
     hungry: true,
-    weight: 0,
-    age: 0,
+    happy: 0,
+    health: 0,
+    energy: 0,
+    sleeping: false,
 }
+
+// actions:
+// set name (once)
+// feed (two options: sweets (+2 happy, +1 energy, -1 health) or veg (+2 health +2 energy -1 happy))
+// play (+2 happy, -1 energy)
+// exercise (+2 health, +1 happy, -2 energy)
 
 function feed() {
     if (pet.hungry) {
@@ -17,17 +25,22 @@ function feed() {
     }
 }
 
+function makeName() {
+    let n = document.getElementById("nameBar").value;
+    console.log(n);
+    pet.name = n;
+    let na = document.getElementById("nameBar");
+    na.style.visibility = "hidden";
+}
 
 function checkStats() {
+    const st = document.getElementById("stats");
+    st.innerHTML = (" ");
     for (const property in pet) {
-        const st = document.getElementById("stats");
         let p = document.createElement("li");
         p.innerHTML = `${property}: ${pet[property]}`;
         st.appendChild(p);
     }
 }
 
-function setName () {
-    pet.name = "milton";
-}
 
